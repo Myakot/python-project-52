a5d584b63b6831c29c1a3a779b79631a34c5fbaf
Myakot <cstepan@yandex.ru>
2024-05-17 20:00:52 +0700

    adding starter tests to check if works and secrets for it

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,6 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+# Don't forget to add views.py file
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    # path('login/', views.login_view, name='login'),
+    # path('logout/', views.logout_view, name='logout'),
+    # path('register/', views.register_view, name='register'),
+    # path('create/', views.create_view, name='create'),
+    # path('task/<int:task_id>/', views.task_view, name='task'),
+    # path('task/<int:task_id>/edit/', views.edit_view, name='edit'),
 ]

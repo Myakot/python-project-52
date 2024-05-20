72a81a7d20508d62b01f369d2a2e64a976ac7fd7
Myakot <cstepan@yandex.ru>
2024-05-20 23:51:41 +0700

    adding templates, fixing django settings

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    extra_context = {'title': _('Task manager')}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context

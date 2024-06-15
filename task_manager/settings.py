af23cd23b39f462b0e9bd7aebe5549aa0786782f
Myakot <cstepan@yandex.ru>
2024-06-16 00:35:41 +0700

    hexlet checker requires downgraded version of rollbar, trying to downgrade it in poetry

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -134,6 +134,7 @@ FIXTURE_DIRS = ('task_manager/fixtures/',)
 ROLLBAR = {
     'access_token': os.getenv('ROLLBAR_ACCESS_TOKEN'),
     'environment': 'development' if DEBUG else 'production',
+    'code_version': '1.0',
     'root': BASE_DIR,
 }
 
